4f3b5bba8dc2bc07d504edd89b3b74d4727388d8
Luis Paolini <paoliniluis@gmail.com>
2022-12-14 12:39:19 -0300

    Rebasing against master

--- dbs/mongo/data/load.sh
+++ dbs/mongo/data/load.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Data loader for MongoDB
-for table in people products orders reviews _metabase_metadata; do
+for table in people products orders reviews _metabase-metadata; do
     echo "Loading ${table}"
     mongoimport --drop --db sample --collection ${table} --type tsv \
         --file "/docker-entrypoint-initdb.d/sample-${table}.tsv" \
