84fdc9f8194aae5773eb63b26b5c74bec18d0ce6
Luis Paolini <paoliniluis@gmail.com>
2023-07-03 13:37:06 -0300

    Adding new data to Mongo Collections

--- dbs/mongo/data/pg_to_mongo.sql
+++ dbs/mongo/data/pg_to_mongo.sql
@@ -61,3 +61,58 @@ COPY (
         value AS "value.string()"
     FROM _metabase_metadata
 ) TO '/tmp/sample-_metabase_metadata.tsv' WITH (FORMAT CSV, HEADER TRUE, DELIMITER E'\t');
+
+COPY (
+    SELECT
+        id AS "id.int64()",
+        email AS "email.string()",
+        first_name AS "first_name.string()",
+        last_name AS "last_name.string()",
+        plan AS "plan.string()",
+        source AS "source.string()",
+        seats AS "seats.int32()",
+        created_at AS "created_at.date(2006-03-02 15:04:05.99999999)",
+        trial_ends_at AS "trial_ends_at.date(2006-03-02 15:04:05.99999999)",
+        canceled_at AS "canceled_at.date(2006-03-02 15:04:05.99999999)",
+        trial_converted AS "trial_converted.boolean()",
+        active_subscription AS "active_subscription.boolean()",
+        legacy_plan AS "legacy_plan.boolean()",
+        latitude AS "latitude.double()",
+        longitude AS "longitude.double()",
+        country AS "country.string()",
+    FROM accounts
+) TO '/tmp/sample-accounts.tsv' WITH (FORMAT CSV, HEADER TRUE, DELIMITER E'\t');
+
+COPY (
+    SELECT
+        id AS "id.int64()",
+        account_id AS "account_id.int64()",
+        event AS "event.string()",
+        timestamp AS "timestamp.date(2006-03-02 15:04:05.99999999)",
+        page_url AS "page_url.string()",
+        button_label AS "button_label.string()",
+        FROM analytic_events
+) TO '/tmp/sample-analytic_events.tsv' WITH (FORMAT CSV, HEADER TRUE, DELIMITER E'\t');
+
+COPY (
+    SELECT
+        id AS "id.int64()",
+        account_id AS "account_id.int64()",
+        email,
+        date_received,
+        rating,
+        rating_mapped,
+        body
+    FROM feedback
+) TO '/tmp/sample-feedback.tsv' WITH (FORMAT CSV, HEADER TRUE, DELIMITER E'\t');
+
+COPY (
+    SELECT 
+        id AS "id.int64()",
+        account_id AS "account_id.int64()",
+        payment AS "payment.int32()",
+        expected_invoice AS "expected_invoice.boolean()",
+        plan as "plan.string()",
+        date_received AS "date_received.date(2006-03-02 15:04:05.99999999)",
+    FROM invoices
+) TO '/tmp/sample-invoices.tsv' WITH (FORMAT CSV, HEADER TRUE, DELIMITER E'\t');
